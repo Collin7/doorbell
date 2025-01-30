8ce8adce6c719566f03387e4074ac9a8cb7e996d
Collin <collin.dewitt@fnb.co.za>
2025-01-30 13:03:26 +0200

    Adding example files

---
+++ MqttTopics_Example.h
@@ -0,0 +1,9 @@
+#ifndef MQTT_TOPICS_H
+#define MQTT_TOPICS_H
+
+// Replace with your actual topics:
+const char* doorbellPressedTopic = "your/topic/goes/here";
+const char* restartTopic = "your/topic/goes/here";
+const char* residentsAwayTopic = "your/topic/goes/here";
+
+#endif
